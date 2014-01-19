20daa7155535395110da86fc6b4a81b57614bad7
remogatto <andrea.fazzi@alcacoop.it>
2014-01-19 17:07:02 +0100

    Refactor tests.go

--- test/src/testlib/tests.go
+++ test/src/testlib/tests.go
@@ -2,6 +2,7 @@ package testlib
 
 import (
 	"fmt"
+	"image"
 	"image/color"
 	"image/png"
 	"path/filepath"
@@ -12,6 +13,32 @@ import (
 	gl "github.com/remogatto/opengles2"
 	"github.com/remogatto/shapes"
 )
+
+const (
+	distanceThreshold = 0.001
+)
+
+// Compare the result of rendering against the saved expected image.
+func testImage(filename string, act image.Image) (float64, image.Image, image.Image, error) {
+	request := mandala.LoadAssetRequest{
+		Filename: filepath.Join(expectedImgPath, filename),
+		Response: make(chan mandala.LoadAssetResponse),
+	}
+
+	mandala.AssetManager() <- request
+	response := <-request.Response
+	buffer := response.Buffer
+
+	if response.Error != nil {
+		return 1, nil, nil, response.Error
+	}
+
+	exp, err := png.Decode(buffer)
+	if err != nil {
+		return 1, nil, nil, err
+	}
+	return imagetest.CompareDistance(exp, act), exp, act, nil
+}
 
 func (t *TestSuite) TestBox() {
 	filename := "expected_box.png"
@@ -27,26 +54,13 @@ func (t *TestSuite) TestBox() {
 		t.testDraw <- testlib.Screenshot(t.renderState.window)
 		t.renderState.window.SwapBuffers()
 	}
-	request := mandala.LoadAssetRequest{
-		Filename: filepath.Join(expectedImgPath, "expected_box.png"),
-		Response: make(chan mandala.LoadAssetResponse),
+	distance, exp, act, err := testImage(filename, <-t.testDraw)
+	if err != nil {
+		panic(err)
 	}
-
-	mandala.AssetManager() <- request
-	response := <-request.Response
-	buffer := response.Buffer
-
-	t.True(response.Error == nil, "An error occured during resource opening")
-
-	if buffer != nil {
-		exp, err := png.Decode(buffer)
-		t.True(err == nil, "An error occured during png decoding")
-		act := <-t.testDraw
-		distance := imagetest.CompareDistance(exp, act)
-		t.True(distance < 0.001, fmt.Sprintf("Distance is %f", distance))
-		if t.Failed() {
-			saveExpAct("failed_"+filename, exp, act)
-		}
+	t.True(distance < distanceThreshold, fmt.Sprint("Image differs, result saved in %s", outputPath))
+	if t.Failed() {
+		saveExpAct("failed_"+filename, exp, act)
 	}
 }
 
@@ -60,32 +74,42 @@ func (t *TestSuite) TestRotatedBox() {
 		box.Color = color.RGBA{1.0, 0.0, 0.0, 1.0}
 		box.AttachToWorld(world)
 		// Rotate the box 20 degrees
-		box.Rotate(20.0)
+		box.Rotate(-20.0)
 		gl.Clear(gl.COLOR_BUFFER_BIT)
 		box.Draw()
 		t.testDraw <- testlib.Screenshot(t.renderState.window)
 		t.renderState.window.SwapBuffers()
 	}
-	request := mandala.LoadAssetRequest{
-		Filename: filepath.Join(expectedImgPath, "expected_box_rotated_20.png"),
-		Response: make(chan mandala.LoadAssetResponse),
+	distance, exp, act, err := testImage(filename, <-t.testDraw)
+	if err != nil {
+		panic(err)
 	}
+	t.True(distance < distanceThreshold, fmt.Sprint("Image differs, result saved in %s", outputPath))
+	if t.Failed() {
+		saveExpAct("failed_"+filename, exp, act)
+	}
+}
 
-	mandala.AssetManager() <- request
-	response := <-request.Response
-	buffer := response.Buffer
-
-	t.True(response.Error == nil, "An error occured during resource opening")
-
-	if buffer != nil {
-		exp, err := png.Decode(buffer)
-		t.True(err == nil, "An error occured during png decoding")
-		act := <-t.testDraw
-		distance := imagetest.CompareDistance(exp, act)
-		t.True(distance < 0.001, fmt.Sprintf("Distance is %f", distance))
-
-		if t.Failed() {
-			saveExpAct("failed_"+filename, exp, act)
-		}
+func (t *TestSuite) TestTranslatedBox() {
+	filename := "expected_box_translated_10_10.png"
+	t.rlControl.drawFunc <- func() {
+		w, h := t.renderState.window.GetSize()
+		world := newWorld(w, h)
+		// Place a box on the center of the window
+		box := shapes.NewBox(0, 0, 100, 100)
+		box.Color = color.RGBA{1.0, 0.0, 0.0, 1.0}
+		box.AttachToWorld(world)
+		gl.Clear(gl.COLOR_BUFFER_BIT)
+		box.Draw()
+		t.testDraw <- testlib.Screenshot(t.renderState.window)
+		t.renderState.window.SwapBuffers()
+	}
+	distance, exp, act, err := testImage(filename, <-t.testDraw)
+	if err != nil {
+		panic(err)
+	}
+	t.True(distance < distanceThreshold, fmt.Sprint("Image differs, result saved in %s", outputPath))
+	if t.Failed() {
+		saveExpAct("failed_"+filename, exp, act)
 	}
 }
